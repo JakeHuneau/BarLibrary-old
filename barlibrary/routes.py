ac20a83fa3196712af94d24edf27e75146bb0460
JakeHuneau <jakehuneau@yahoo.com>
2018-10-28 18:20:41 -0400

    Added jinja pages for add, edit, remove, find recipe and added routes for those

--- barlibrary/routes.py
+++ barlibrary/routes.py
@@ -3,3 +3,7 @@ def includeme(config):
 
     # Home page which will have buttons add_recipe and search_recipes
     config.add_route('home', '/')
+    config.add_route('add_recipe', 'add_recipe')
+    config.add_route('edit_recipe', 'edit_recipe')
+    config.add_route('remove_recipe', 'remove_recipe')
+    config.add_route('find_recipes', 'find_recipes')
---
+++ barlibrary/views/views.py
@@ -0,0 +1,27 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+
+from sqlalchemy.exc import DBAPIError
+
+from barlibrary.models import Recipe
+
+
+@view_config(route_name='home', renderer='../templates/home.jinja2')
+def home_view(request):
+    return {}
+
+@view_config(route_name='add_recipe', renderer='../templates/add_recipe.jinja2')
+def add_recipe_view(request):
+    return {}
+
+@view_config(route_name='edit_recipe', renderer='../templates/edit_recipe.jinja2')
+def edit_recipe_view(request):
+    return {}
+
+@view_config(route_name='remove_recipe', renderer='../templates/remove_recipe.jinja2')
+def remove_recipe_view(request):
+    return {}
+
+@view_config(route_name='find_recipes', renderer='../templates/find_recipes.jinja2')
+def find_recipes_view(request):
+    return {}
